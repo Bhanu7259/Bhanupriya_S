c176913dd3e4fd97690a04114ac2a261d99c0e9d
Bhanu7259 <86303339+Bhanu7259@users.noreply.github.com>
2021-07-12 14:42:57 +0530

    Create delay.c

---
+++ 3_Implementation/src/delay.c
@@ -0,0 +1,11 @@
+#include "tic_tac_toe.h"
+
+// generates a delay to implicate the time for the user to enter the token
+void delay(long long int j)
+{
+    int i,k;
+    for(i=0;i<j;i++)
+    {
+        k=i;
+    }
+}
